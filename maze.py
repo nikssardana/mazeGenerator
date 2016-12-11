494d24978e5d1168e38951d82719afa2e308c674
Nikhil Sardana <nikhilklk@gmail.com>
2016-12-11 21:50:53 +0530

    select neighbour created;color differently the current box is buggy

--- maze.py
+++ maze.py
@@ -1,6 +1,7 @@
 # Import a library of functions called 'pygame'
 import pygame
 from math import pi
+import random
 
 # Initialize the game engine
 pygame.init()
@@ -30,11 +31,15 @@ class Point(object):
 
 grid = [ [ Point() for x in range(cols) ] for y in range(cols) ] #A grid of points ie rectangular boxes
 grid[0][0].visited = True
-current = grid[0][0]
+currentX = 0
+currentY = 0
 
 def drawGrid():
+    global currentX,currentY
     for x in range(cols):
         for y in range(cols):
+            if currentX == x and currentY == y:
+                pygame.draw.rect(screen, GREEN, [(x)*width, (y)*width, (x+1)*width, (y+1)*width])
             #TBRL
             if grid[x][y].draw[0]:
                 pygame.draw.line(screen, BLACK, [(x)*width,(y)*width], [(x+1)*width,(y)*width], 5)
@@ -47,14 +52,40 @@ def drawGrid():
 
             if grid[x][y].visited:
                 pygame.draw.rect(screen, BLUE, [(x)*width, (y)*width, (x+1)*width, (y+1)*width])
-            if current == grid[x][y]:
-                pygame.draw.rect(screen, GREEN, [(x)*width, (y)*width, (x+1)*width, (y+1)*width])
+    #Set current box as visited
+    grid[currentX][currentY].visited = True
+    #Choose a next neighbour that has not yet been visited and set it as the current point
+    nextX,nextY = selectNeighbour(currentX,currentY)
+    if nextX != -1:
+        currentX = nextX
+        currentY = nextY
+
+def selectNeighbour(x,y):
+    #Select a random neighbour out of TBRL that has not been visited and return it
+    neighbours = []
+    #Top neighbour
+    if y>0 and not grid[x][y-1].visited:
+        neighbours.append( [x,y-1] )
+    #Bottom neighbour
+    if y<cols-1 and not grid[x][y+1].visited:
+        neighbours.append( [x,y+1] )
+    #Left neighbour
+    if x>0 and not grid[x-1][y].visited:
+        neighbours.append( [x-1,y] )
+    #Right neighbour
+    if x<cols-1 and not grid[x+1][y].visited:
+        neighbours.append( [x+1,y] )
+
+    if not len(neighbours):
+        return( [ -1,-1 ] )
+    n = neighbours[ random.randint(0,len(neighbours)-1) ]
+    return n
 
 
 while not done:
     # This limits the while loop to a max of 10 times per second.
     # Leave this out and we will use all CPU we can.
-    clock.tick(10)
+    clock.tick(2)
     #Exit when user clicks close
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
