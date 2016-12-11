b715d894c74de6260788f9607020c8937929edb1
Nikhil Sardana <nikhilklk@gmail.com>
2016-12-11 21:18:24 +0530

    draw simple maze

--- maze.py
+++ maze.py
@@ -11,7 +11,7 @@ WHITE = (255, 255, 255)
 BLUE =  (  0,   0, 255)
 
 # Set the height and width of the screen
-size = [400, 300]
+size = [500, 500]
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("Maze")
@@ -19,6 +19,17 @@ pygame.display.set_caption("Maze")
 #Loop until the user clicks the close button.
 done = False
 clock = pygame.time.Clock()
+cols = 10
+width = int(size[0]/cols)
+
+def drawGrid():
+    for x in range(cols):
+        for y in range(cols):
+            #TBRL
+            pygame.draw.line(screen, BLACK, [(x)*width,(y)*width], [(x+1)*width,(y)*width], 5)
+            pygame.draw.line(screen, BLACK, [(x)*width,(y+1)*width], [(x+1)*width,(y+1)*width], 5)
+            pygame.draw.line(screen, BLACK, [(x+1)*width,(y)*width], [(x+1)*width,(y+1)*width], 5)
+            pygame.draw.line(screen, BLACK, [(x)*width,(y)*width], [(x)*width,(y+1)*width], 5)
 
 while not done:
     # This limits the while loop to a max of 10 times per second.
@@ -30,7 +41,8 @@ while not done:
             done=True # Flag that we are done so we exit this loop
     # Clear the screen and set the screen background
     screen.fill(WHITE)
-    pygame.draw.line(screen, BLACK, [0, 0], [50,30], 5)
+
+    drawGrid()
     # Go ahead and update the screen with what we've drawn.
     # This MUST happen after all the other drawing commands.
     pygame.display.flip()
